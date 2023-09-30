76112ccc9dafcade1f00b296b669f840994b1ff1
Juanvi1905 <146494823+Juanvi1905@users.noreply.github.com>
2023-09-29 19:15:31 -0500

    Add files via upload

---
+++ suma.py
@@ -0,0 +1,6 @@
+Num1 = int(input ("Por favor ingrese el primer numero a sumar "))
+Num2 = int(input ("Por favor ingrese el segundo numero a sumar "))
+
+resultado = Num1 + Num2
+
+print ("El resultado de la suma es:", resultado)
